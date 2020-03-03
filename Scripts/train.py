434483ea9209d81e92b29d6d7829562ed9519db6
Luis Chaves <lc5415@cx1-101-16-3.cx1.hpc.ic.ac.uk>
2020-03-03 17:11:58 +0000

    All the way till running 4 parallel jobs in cluster

--- Scripts/train.py
+++ Scripts/train.py
@@ -28,6 +28,7 @@ import pandas as pd
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     train_loss = 0
+    lossPerBatch = pd.DataFrame(columns=["epoch","batchNum", "Train_loss"])
     for batch_idx, batch in enumerate(train_loader):
         # get the inputs in correct format and pass them onto CPU/GPU
         data, target = batch['image'].to(device), batch['age'].to(device)
@@ -59,12 +60,12 @@ def train(args, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * train_loader.batch_sampler.batch_size,
                 len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.item()))
-
+        lossPerBatch.loc[len(lossPerBatch)] = [epoch, batch_idx, loss.item()]
         #we return the averaged train loss over all the batches over all the samples, this is actually 
         # like the mean. train loss is SSE, the thing we are dividing by is N.
         
         # we would prefer to output RMSE to have everything on same scale
-    return train_loss/len(train_loader.dataset)
+    return train_loss/len(train_loader.dataset), lossPerBatch
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -102,7 +103,7 @@ def main():
 
 
     ## my code does not really take this into account
-    parser.add_argument('--batch-size', type=int, default=8, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                         help='input batch size for training (default: 64)')
 
     # nor does it takes this into account
@@ -110,7 +111,7 @@ def main():
                         help='input batch size for testing (default: 1000)')
 
     ## will likely need to change this
-    parser.add_argument('--epochs', type=int, default=3, metavar='N',
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 14)')
 
     ## may need to do CV for this
@@ -129,7 +130,12 @@ def main():
     ## may want to do this when submitting multi-node jobs for cross-val
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
+    ######### CUSTOM args
+    parser.add_argument('--arch', type = int, default = 1,
+                           help = 'ResNet architecture choice')
     args = parser.parse_args()
+    
+    
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
@@ -139,7 +145,7 @@ def main():
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
-
+    print(args)
     
     
     # loading premade dataloaders
@@ -175,17 +181,14 @@ def main():
                      (BasicBlock, [3, 4, 6, 3]),
                      (Bottleneck, [3, 4, 6, 3]),
                      (Bottleneck, [3, 4, 23, 3])]
-
-    if sys.argv != ['']:
-        chosenArch = architectures[0]
-    else:
-        arch_choice = sys.argv[1]
-        chosenArch = architectures[arch_choice]
+    
+    # here is where the input from the command line comes in
+    chosenArch = architectures[args.arch-1]
     # set architecture from bash script call
     net = ResNet(chosenArch[0], chosenArch[1], num_classes=1)
     net = net.double()
     model = net.to(device)
-    print("You have loaded a ResNet with {} blocks and {} layers".format(str(BasicBlock), str(chosenArch[1])))
+    print("You have loaded a ResNet with {} blocks and {} layers".format(str(chosenArch[0]), str(chosenArch[1])))
     # does the optimizer I use matter?
     # optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
@@ -194,9 +197,9 @@ def main():
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
 
     Loss_monitor = pd.DataFrame(columns=["Train loss", "Test loss"])
-
+    trainLossBatch = pd.DataFrame(columns=["epoch", "batchNum", "Train Loss"])
     for epoch in range(1, args.epochs + 1):
-        train_loss = train(args, model, device, train_loader, optimizer, epoch)
+        train_loss, lossPerBatch = train(args, model, device, train_loader, optimizer, epoch)
         test_loss = test(args, model, device, test_loader)
         
         
@@ -214,15 +217,17 @@ def main():
 #                 opts=dict(xlabel='Epochs', ylabel='Loss', title='Training Loss', legend=['Loss']))
 
         Loss_monitor.loc[len(Loss_monitor)] = [train_loss, test_loss]
+        trainLossBatch = trainLossBatch.append(lossPerBatch, ignore_index = True)
         scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), "HtoA.pt")
 
-    Loss_monitor.to_csv("Results/Lossframe.csv")
+    Loss_monitor.to_csv("Results/TrainTestLoss"+str(args.arch)+".csv")
+    trainLossBatch.to_csv("Results/LossPBatch"+str(args.arch)+".csv")
+    
 
 if __name__ == "__main__":
     # NOT USING VISDOM FOR NOW
     # viz = visdom.Visdom(port = 8097)
-    print(os.listdir())
     main()
