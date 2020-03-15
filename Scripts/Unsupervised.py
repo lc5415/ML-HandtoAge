d751e981089b1669f3a7a0aa8fbfeb997c0882d1
Luis Chaves <lc5415@cx1-101-16-3.cx1.hpc.ic.ac.uk>
2020-03-15 22:40:11 +0000

    PCA compression did not run but kmeans did

--- Scripts/Unsupervised.py
+++ Scripts/Unsupervised.py
@@ -36,137 +36,137 @@ use_cuda = cuda.is_available()
 
 print("You are using a {}".format("cuda" if use_cuda else "cpu"))
 
-# preprocessing images
-if platform.system() == 'Linux':
-    train_loader = getData("FULLdata/training",
-                           "boneage-training-dataset.csv",
-                           transform=transforms.Compose(
-                               [Rescale(256),
-                                # RandomCrop(224),
-                                CenterCrop(224),
-                                CHALE(),
-                                InstanceNorm(),
-                                ToTensor()
-                                ]),
-                           batch_size="full", plot=0, save=0)
+# # preprocessing images
+# if platform.system() == 'Linux':
+#     train_loader = getData("FULLdata/training",
+#                            "boneage-training-dataset.csv",
+#                            transform=transforms.Compose(
+#                                [Rescale(256),
+#                                 # RandomCrop(224),
+#                                 CenterCrop(224),
+#                                 CHALE(),
+#                                 InstanceNorm(),
+#                                 ToTensor()
+#                                 ]),
+#                            batch_size="full", plot=0, save=0)
 
-    test_loader = getData("FULLdata/test",
-                          "boneage-training-dataset.csv",
-                          transform=transforms.Compose(
-                              [Rescale(256),
-                               # RandomCrop(224),
-                               CenterCrop(224),
-                               CHALE(),
-                               InstanceNorm(),
-                               ToTensor()
-                               ]),
-                          batch_size="full", plot=0, save=0)
+#     test_loader = getData("FULLdata/test",
+#                           "boneage-training-dataset.csv",
+#                           transform=transforms.Compose(
+#                               [Rescale(256),
+#                                # RandomCrop(224),
+#                                CenterCrop(224),
+#                                CHALE(),
+#                                InstanceNorm(),
+#                                ToTensor()
+#                                ]),
+#                           batch_size="full", plot=0, save=0)
 
-else:
-    ## LOAD DATA -- on the fly
-    train_loader = getData("labelled/train/",
-                           "boneage-training-dataset.csv",
-                           transform=transforms.Compose(
-                               [Rescale(256),
-                                # RandomCrop(224),
-                                CenterCrop(224),
-                                CHALE(),
-                                InstanceNorm(),
-                                ToTensor()
-                                ]),
-                           plot=0, batch_size="full")
+# else:
+#     ## LOAD DATA -- on the fly
+#     train_loader = getData("labelled/train/",
+#                            "boneage-training-dataset.csv",
+#                            transform=transforms.Compose(
+#                                [Rescale(256),
+#                                 # RandomCrop(224),
+#                                 CenterCrop(224),
+#                                 CHALE(),
+#                                 InstanceNorm(),
+#                                 ToTensor()
+#                                 ]),
+#                            plot=0, batch_size="full")
 
-    test_loader = getData("labelled/test/",
-                          "boneage-training-dataset.csv",
-                          transform=transforms.Compose(
-                              [Rescale(256),
-                               # RandomCrop(224),
-                               CenterCrop(224),
-                               CHALE(),
-                               InstanceNorm(),
-                               ToTensor()
-                               ]),
-                          plot=0, batch_size="full")
+#     test_loader = getData("labelled/test/",
+#                           "boneage-training-dataset.csv",
+#                           transform=transforms.Compose(
+#                               [Rescale(256),
+#                                # RandomCrop(224),
+#                                CenterCrop(224),
+#                                CHALE(),
+#                                InstanceNorm(),
+#                                ToTensor()
+#                                ]),
+#                           plot=0, batch_size="full")
 
-numTrain = len(train_loader)*train_loader.batch_size
-# Initialise X_train as empty array of shape:
-# (number of rows of TrainData = 600)x(number of elements in flatten vector (32*32*3))
-# to hold the training features,
-X_train = np.empty([numTrain, 224*224])
-y_train = np.empty([numTrain])
-# iterate through the filenames in the labelling list so that
-# Xtrain and labels are in same order
-i = 0  # indexing variable i
-for batch_id, (batch, _, gender) in enumerate(train_loader):
-    # reshape image as done previously and store in X_train ith row
-    for image in range(batch.shape[0]):
-        X_train[i, :] = batch[image].flatten()
-        y_train[i] = gender[image]
-        # update indexing variable i
-        i += 1
+# numTrain = len(train_loader)*train_loader.batch_size
+# # Initialise X_train as empty array of shape:
+# # (number of rows of TrainData = 600)x(number of elements in flatten vector (32*32*3))
+# # to hold the training features,
+# X_train = np.empty([numTrain, 224*224])
+# y_train = np.empty([numTrain])
+# # iterate through the filenames in the labelling list so that
+# # Xtrain and labels are in same order
+# i = 0  # indexing variable i
+# for batch_id, (batch, _, gender) in enumerate(train_loader):
+#     # reshape image as done previously and store in X_train ith row
+#     for image in range(batch.shape[0]):
+#         X_train[i, :] = batch[image].flatten()
+#         y_train[i] = gender[image]
+#         # update indexing variable i
+#         i += 1
 
-numTest = len(test_loader)*test_loader.batch_size
-X_test = np.empty([numTest, 224*224])
-y_test = np.empty([numTest])
-# iterate through the filenames in the labelling list so that
-# Xtrain and labels are in same order
-i = 0  # indexing variable i
-for batch_id, (batch, _, gender) in enumerate(test_loader):
-    # reshape image as done previously and store in X_train ith row
-    for image in range(batch.shape[0]):
-        X_test[i, :] = batch[image].flatten()
-        y_test[i] = gender[image]
-        # update indexing variable i
-        i += 1
+# numTest = len(test_loader)*test_loader.batch_size
+# X_test = np.empty([numTest, 224*224])
+# y_test = np.empty([numTest])
+# # iterate through the filenames in the labelling list so that
+# # Xtrain and labels are in same order
+# i = 0  # indexing variable i
+# for batch_id, (batch, _, gender) in enumerate(test_loader):
+#     # reshape image as done previously and store in X_train ith row
+#     for image in range(batch.shape[0]):
+#         X_test[i, :] = batch[image].flatten()
+#         y_test[i] = gender[image]
+#         # update indexing variable i
+#         i += 1
 
 
-kmeans = KMeans(n_clusters = 2).fit(X_train)
-y_predict = kmeans.predict(X_test)
+# kmeans = KMeans(n_clusters = 2).fit(X_train)
+# y_predict = kmeans.predict(X_test)
 
-plt.scatter(X_train[:,0], X_train[:,1], c = y_train, cmap = 'viridis')
-plt.show()
+# plt.scatter(X_train[:,0], X_train[:,1], c = y_train, cmap = 'viridis')
+# plt.show()
 
-## PCA for to decorrelate data and then do kmeans
-pca_plot = PCA()
-pca_plot.fit(X_train)
-comp_to_keep = np.sum(np.cumsum(pca_plot.explained_variance_ratio_)<0.9)
-pca_plot = PCA(n_components = comp_to_keep)
-pca_plot.fit(X_train)
-X_proj = pca_plot.transform(X_train)
-kmeans2 = KMeans(n_clusters = 2).fit(X_proj)
+# ## PCA for to decorrelate data and then do kmeans
+# pca_plot = PCA()
+# pca_plot.fit(X_train)
+# comp_to_keep = np.sum(np.cumsum(pca_plot.explained_variance_ratio_)<0.9)
+# pca_plot = PCA(n_components = comp_to_keep)
+# pca_plot.fit(X_train)
+# X_proj = pca_plot.transform(X_train)
+# kmeans2 = KMeans(n_clusters = 2).fit(X_proj)
 
-y_train_str = ["Male" if np.array(kmeans.labels_[i]) == 1 else "Female" for i in range(len(kmeans2.labels_))]
+# y_train_str = ["Male" if np.array(kmeans.labels_[i]) == 1 else "Female" for i in range(len(kmeans2.labels_))]
 
-data = (X_proj[y_train==1,:], X_proj[y_train==0,:])
-colors = ("darkblue", "lightblue")
-groups = ("Male", "Female")
+# data = (X_proj[y_train==1,:], X_proj[y_train==0,:])
+# colors = ("darkblue", "lightblue")
+# groups = ("Male", "Female")
 
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
-for data, color, group in zip(data,colors, groups):
-    x = data
-    ax.scatter(x[:,0], x[:,1], c = color, label = group)
-plt.legend()
-plt.title("Projection of points onto PCA top-2 components")
-plt.grid()
-plt.savefig("Images/PCAProject.png")
-plt.show()
-print("Components kept for 90% explained variance: {}".format(comp_to_keep))
-########## Second plot
-data = (X_proj[kmeans2.labels_==1,:], X_proj[kmeans2.labels_==0,:])
-colors = ("darkblue", "lightblue")
-groups = ("Male", "Female")
+# for data, color, group in zip(data,colors, groups):
+#     x = data
+#     ax.scatter(x[:,0], x[:,1], c = color, label = group)
+# plt.legend()
+# plt.title("Projection of points onto PCA top-2 components")
+# plt.grid()
+# plt.savefig("Images/PCAProject.png")
+# plt.show()
+# print("Components kept for 90% explained variance: {}".format(comp_to_keep))
+# ########## Second plot
+# data = (X_proj[kmeans2.labels_==1,:], X_proj[kmeans2.labels_==0,:])
+# colors = ("darkblue", "lightblue")
+# groups = ("Male", "Female")
 
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
-for data, color, group in zip(data,colors, groups):
-    x = data
-    ax.scatter(x[:,0], x[:,1], c = color, label = group)
-plt.legend()
-plt.grid()
-plt.title("Clustering by K-means on PCA-projected data")
-plt.savefig("Images/KmeansCluster.png")
-plt.show()
+# for data, color, group in zip(data,colors, groups):
+#     x = data
+#     ax.scatter(x[:,0], x[:,1], c = color, label = group)
+# plt.legend()
+# plt.grid()
+# plt.title("Clustering by K-means on PCA-projected data")
+# plt.savefig("Images/KmeansCluster.png")
+# plt.show()
 
 ##PCA for image compression down here
 
@@ -182,7 +182,7 @@ if platform.system() == 'Linux':
                                 InstanceNorm(),
                                 ToTensor()
                                 ]),
-                           batch_size=args.batch_size, plot=0, save=0)
+                           batch_size="full", plot=0, save=0)
 
     test_loader = getData("FULLdata/test",
                           "boneage-training-dataset.csv",
@@ -194,7 +194,7 @@ if platform.system() == 'Linux':
                                InstanceNorm(),
                                ToTensor()
                                ]),
-                          batch_size=args.test_batch_size, plot=0, save=0)
+                          batch_size="full", plot=0, save=0)
 
 else:
     ## LOAD DATA -- on the fly
